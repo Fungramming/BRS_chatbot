b48806ccfec4e6ebb31c6594a6714fbe4b108b80
94wogus <94wogus@gmail.com>
2018-10-10 20:12:00 +0900

    배송 조회 API 상품 이미지 추가 완료

--- cafe24_app/api/UrlHelper.py
+++ cafe24_app/api/UrlHelper.py
@@ -11,26 +11,36 @@ from fake_useragent import UserAgent
 ua = UserAgent()
 
 # 회원의 주문내역을 조회하기 위한 URL(최대 1개원 이내의 주문)
-def get_order_request_url(MallId, member_id, AccessToken):
+def get_orders_request_url(MallId, member_id, AccessToken):
     start, end = orders_date_range()
 
     request_url = 'https://' + MallId + '.' + current_app.config['REQUEST_BASE_PATH'] + \
-                  '/orders?start_date=' + start + '&end_date=' + end + '&member_id=' + member_id + '&date_type=order_date'
+                  '/orders?start_date=' + start + '&end_date=' + end + '&member_id=' + member_id + '&date_type=order_date' + '&order_status=N00,N10,N20,N21,N22,N30' + '&limit=500'
     headers = {'Authorization': 'Bearer' + ' ' + AccessToken, 'Content-Type': 'application/json',
                'User-Agent': ua.random}
 
     return request_url, headers
 
-# 회원의 특정 상품의 배송 상태를 조회하기 위한 URL
-def specific_shipping_status_request_url(MallId, member_id, order_id, AccessToken):
+# 회원의 특정 주문의 배송 상태를 조회하기 위한 URL
+def get_orders_items_request_url(MallId, order_id, AccessToken):
     start, end = orders_date_range()
 
-    request_url = 'https://' + MallId + '.' + current_app.config['REQUEST_BASE_PATH'] + \
-                  '/orders?start_date=' + start + '&end_date=' + end + '&member_id=' + member_id + '&order_id=' + order_id + '&date_type=order_date'
+    request_url = 'https://' + MallId + '.' + current_app.config['REQUEST_BASE_PATH'] + '/orders/' + order_id + '/items'
     headers = {'Authorization': 'Bearer' + ' ' + AccessToken, 'Content-Type': 'application/json',
                'User-Agent': ua.random}
 
     return request_url, headers
+
+def get_products_request_url(MallId, AccessToken, product_num_str):
+    if product_num_str == None:
+        query = None
+    else:
+        query = '?product_no=' + product_num_str
+    request_url = 'https://' + MallId + '.' + current_app.config['REQUEST_BASE_PATH'] + '/products' + query
+    headers = {'Authorization': 'Bearer' + ' ' + AccessToken, 'Content-Type': 'application/json',
+               'User-Agent': ua.random}
+
+    return  request_url, headers
 
 # 회원의 아이디를 얻기위한 URL
 def get_member_id_request_url(MallId, cellphone, AccessToken):
@@ -39,10 +49,3 @@ def get_member_id_request_url(MallId, cellphone, AccessToken):
                'User-Agent': ua.random}
 
     return request_url, headers
-
-def get_products_request_url(MallId, AccessToken):
-    request_url = 'https://' + MallId + '.' + current_app.config['REQUEST_BASE_PATH'] + '/products'
-    headers = {'Authorization': 'Bearer' + ' ' + AccessToken, 'Content-Type': 'application/json',
-               'User-Agent': ua.random}
-
-    return  request_url, headers
--- cafe24_app/api/__init__.py
+++ cafe24_app/api/__init__.py
@@ -3,4 +3,3 @@ from flask import Blueprint
 api = Blueprint('api', __name__)
 
 from . import DeliveryTracking, DateHelper, UrlHelper
-# from . import fake
