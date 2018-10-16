77f1fa2352503aa58d4b08568c2af954beb93907
94wogus <94wogus@gmail.com>
2018-10-16 12:24:50 +0900

    api 요청 과부하 문제 해결

--- cafe24_app/api/DeliveryTracking.py
+++ cafe24_app/api/DeliveryTracking.py
@@ -14,36 +14,59 @@ def get_orders_delivering():
     shop_no = request.args.get('shop_no')
     member_id = request.args.get('member_id')
 
-    orders_list = list()
-    orders_status = {'N00': '입급전', 'N10': '상품 준비중', 'N20': '배송 준비중', 'N21': '배송대기', 'N22': '배송보류', 'N30': '배송중'}
-    orders_status_code = 'N00,N10,N20,N21,N22,N30'
-    product_num_list = list()
-    product_image_list = dict()
-
+    # accesstoken 확인 절차 및 주문조회
     MallId, AccessToken = Confirm_access_expiration(mall_id, shop_no)
-    request_url, headers = get_orders_request_url(MallId, member_id, AccessToken, orders_status_code)
+    request_url, headers = get_ondelivering_orders_request_url(MallId, shop_no, member_id, AccessToken)
     response = requests.get(request_url, headers=headers)
-    orders = response.json()['orders']
+    result = response.json()
+
+    if 'error' in result:
+        return jsonify({'error': result['error']})
+
+    orders = result['orders']
 
     if len(orders) == 0:
         return jsonify({'orders': '배송상태에 있는 주문이 없습니다.'})
 
+    orders_list, product_no_list, product_image_list = list(), list(), dict()
+
+    # data 가공처리중(product image 조회를 위한 작업도 동시에 진행)
     for order in orders:
-        request_url, headers = get_orders_items_request_url(MallId, order['order_id'], AccessToken)
-        response = requests.get(request_url, headers=headers)
-        items = response.json()['items']
+        items = order['items']
+        item_list = list()
 
         for item in items:
-            product_num_list.append(item['product_no'])
-            if item['order_status'] == 'N40':
-                pass
-            else:
-                item['order_id'] = order['order_id']
-                item['order_status_message'] = orders_status[item['order_status']]
-                orders_list.append(item)
+            item_json = {'order_item_code': item['order_item_code'],
+                         'order_status': item['order_status'],
+                         'product_code': item['product_code'],
+                         'product_name': item['product_name'],
+                         'product_no': item['product_no'],
+                         'product_price': item['product_price'],
+                         'quantity': item['quantity'],
+                         'z_option': {'option_id': item['option_id'],
+                                    'option_price': item['option_price'],
+                                    'option_value': item['option_value'],
+                                    'option_value_default': item['option_value_default']
+                                    },
+                         'shipping_code': item['shipping_code'],
+                         'shipping_company_code': item['shipping_company_code'],
+                         'shipping_company_id': item['shipping_company_id'],
+                         'shipping_company_name': item['shipping_company_name'],
+                         'status_code': item['status_code'],
+                         'status_text': item['status_text'],
+                         'tracking_no': item['tracking_no']
+                         }
+            item_list.append(item_json)
+            product_no_list.append(item['product_no'])
 
-    product_num_str = ','.join(str(n) for n in set(product_num_list))
-    request_url, headers =  get_products_request_url(MallId, AccessToken, product_num_str)
+        orders_json = {'order_id': order['order_id'],
+                       'order_date': order['order_date'],
+                       'order_items': item_list}
+        orders_list.append(orders_json)
+
+    # product 이미지 조회 및 데이터 가공
+    product_str = ','.join(str(n) for n in set(product_no_list))
+    request_url, headers = get_products_request_url(MallId, shop_no, AccessToken, product_str)
     response = requests.get(request_url, headers=headers)
     products = response.json()['products']
 
@@ -52,8 +75,12 @@ def get_orders_delivering():
                                                           'list_image': product['list_image'],
                                                           'small_image': product['small_image'],
                                                           'tiny_image': product['tiny_image']}
+
+    # product image 삽입
     for order in orders_list:
-        order['image_url'] = product_image_list[int(order['product_no'])]
+        items = order['order_items']
+        for item in items:
+            item['z_product_image_url'] = product_image_list[int(item['product_no'])]
 
     return jsonify({'orders': orders_list})
 
@@ -65,47 +92,64 @@ def get_orders_delivered():
     member_id = request.args.get('member_id')
     page = int(request.args.get('page'))
     per_page = current_app.config['PER_PAGE']
-    list_s = (page - 1) * per_page
-    list_e = list_s + per_page
 
-
-    orders_list, orders, product_num_list, product_image_list = list(), list(), list(), dict()
-    orders_status = {'N40': '배송완료'}
-    orders_status_code = 'N40'
-
+    # accesstoken 확인 절차 및 주문조회
     MallId, AccessToken = Confirm_access_expiration(mall_id, shop_no)
-    request_url, headers = get_orders_request_url(MallId, member_id, AccessToken, orders_status_code)
+    request_url, headers = get_delivered_orders_request_url(MallId, shop_no, member_id, AccessToken, page, per_page)
     response = requests.get(request_url, headers=headers)
-    orders_all = response.json()['orders']
-
-    for i in range(list_s, list_e):
-        print(i)
-        if i >= len(orders_all):
-            next_url = None
-        else:
-            orders.append(orders_all[i])
-            next_url = current_app.config['SERVER_URL'] + url_for('api.get_orders_delivered', mall_id=mall_id, shop_no=shop_no, member_id=member_id, page=page + 1)
-
+    orders = response.json()['orders']
 
     if len(orders) == 0:
-        return jsonify({'orders': '배송완료된 주문이 없습니다.'})
+        return jsonify({'orders': '배송상태에 있는 주문이 없습니다.'})
 
+    if len(orders) < 3:
+        next_url = None
+    else:
+        next_url = current_app.config['SERVER_URL'] + url_for('api.get_orders_delivered', mall_id=mall_id,
+                                                              shop_no=shop_no, member_id=member_id, page=page + 1)
+        orders.pop(-1)
+
+    orders_list = list()
+    product_no_list = list()
+    product_image_list = dict()
+
+    # data 가공처리중(product image 조회를 위한 작업도 동시에 진행)
     for order in orders:
-        request_url, headers = get_orders_items_request_url(MallId, order['order_id'], AccessToken)
-        response = requests.get(request_url, headers=headers)
-        items = response.json()['items']
+        items = order['items']
+        item_list = list()
 
         for item in items:
-            product_num_list.append(item['product_no'])
-            if item['order_status'] != 'N40':
-                pass
-            else:
-                item['order_id'] = order['order_id']
-                item['order_status_message'] = orders_status[item['order_status']]
-                orders_list.append(item)
+            item_json = {'order_item_code': item['order_item_code'],
+                         'order_status': item['order_status'],
+                         'product_code': item['product_code'],
+                         'product_name': item['product_name'],
+                         'product_no': item['product_no'],
+                         'product_price': item['product_price'],
+                         'quantity': item['quantity'],
+                         'option': {'option_id': item['option_id'],
+                                    'option_price': item['option_price'],
+                                    'option_value': item['option_value'],
+                                    'option_value_default': item['option_value_default']
+                                    },
+                         'shipping_code': item['shipping_code'],
+                         'shipping_company_code': item['shipping_company_code'],
+                         'shipping_company_id': item['shipping_company_id'],
+                         'shipping_company_name': item['shipping_company_name'],
+                         'status_code': item['status_code'],
+                         'status_text': item['status_text'],
+                         'tracking_no': item['tracking_no']
+                         }
+            item_list.append(item_json)
+            product_no_list.append(item['product_no'])
 
-    product_num_str = ','.join(str(n) for n in set(product_num_list))
-    request_url, headers = get_products_request_url(MallId, AccessToken, product_num_str)
+        orders_json = {'order_id': order['order_id'],
+                       'order_date': order['order_date'],
+                       'order_items': item_list}
+        orders_list.append(orders_json)
+
+    # product 이미지 조회 및 데이터 가공
+    product_str = ','.join(str(n) for n in set(product_no_list))
+    request_url, headers = get_products_request_url(MallId, shop_no, AccessToken, product_str)
     response = requests.get(request_url, headers=headers)
     products = response.json()['products']
 
@@ -114,12 +158,14 @@ def get_orders_delivered():
                                                           'list_image': product['list_image'],
                                                           'small_image': product['small_image'],
                                                           'tiny_image': product['tiny_image']}
+
+    # product image 삽입
     for order in orders_list:
-        order['image_url'] = product_image_list[int(order['product_no'])]
+        items = order['order_items']
+        for item in items:
+            item['product_image_url'] = product_image_list[int(item['product_no'])]
 
-    print(len(orders_list))
-
-    return jsonify({'orders': orders_list, 'next_url': next_url})
+    return jsonify({'orders': orders_list, 'next_url': next_url, 'total_count': len(orders_list)})
 
 #전화번호를 이용하여 회원의 아이디를 얻기 위한 API
 @api.route('/user/')
@@ -130,7 +176,7 @@ def get_member_id():
 
     MallId, AccessToken = Confirm_access_expiration(mall_id, shop_no)
 
-    request_url, headers = get_member_id_request_url(MallId, cellphone, AccessToken)
+    request_url, headers = get_member_id_request_url(MallId, shop_no, cellphone, AccessToken)
 
     response = requests.get(request_url, headers=headers)
     result = response.json()
@@ -138,65 +184,6 @@ def get_member_id():
     return jsonify(result)
 
 # test하기 위한 API
-# @api.route('/test/')
-# def test():
-#     mall_id = request.args.get('mall_id')
-#     shop_no = request.args.get('shop_no')
-#     member_id = request.args.get('member_id')
-#     page = int(request.args.get('page'))
-#     per_page = current_app.config['PER_PAGE']
-#     list_s = (page - 1) * per_page
-#     list_e = list_s + per_page
-#
-#
-#     orders_list, orders, product_num_list, product_image_list = list(), list(), list(), dict()
-#     orders_status = {'N40': '배송완료'}
-#     orders_status_code = 'N40'
-#
-#     MallId, AccessToken = Confirm_access_expiration(mall_id, shop_no)
-#     request_url, headers = get_orders_request_url(MallId, member_id, AccessToken, orders_status_code)
-#     response = requests.get(request_url, headers=headers)
-#     orders_all = response.json()['orders']
-#
-#     for i in range(list_s, list_e):
-#         print(i)
-#         if i >= len(orders_all):
-#             next_url = None
-#         else:
-#             orders.append(orders_all[i])
-#             next_url = url_for('api.test', mall_id=mall_id, shop_no=shop_no, member_id=member_id, page=page + 1)
-#
-#
-#     if len(orders) == 0:
-#         return jsonify({'orders': '배송완료된 주문이 없습니다.'})
-#
-#     for order in orders:
-#         request_url, headers = get_orders_items_request_url(MallId, order['order_id'], AccessToken)
-#         response = requests.get(request_url, headers=headers)
-#         items = response.json()['items']
-#
-#         for item in items:
-#             product_num_list.append(item['product_no'])
-#             if item['order_status'] != 'N40':
-#                 pass
-#             else:
-#                 item['order_id'] = order['order_id']
-#                 item['order_status_message'] = orders_status[item['order_status']]
-#                 orders_list.append(item)
-#
-#     product_num_str = ','.join(str(n) for n in set(product_num_list))
-#     request_url, headers = get_products_request_url(MallId, AccessToken, product_num_str)
-#     response = requests.get(request_url, headers=headers)
-#     products = response.json()['products']
-#
-#     for product in products:
-#         product_image_list[int(product['product_no'])] = {'detail_image': product['detail_image'],
-#                                                           'list_image': product['list_image'],
-#                                                           'small_image': product['small_image'],
-#                                                           'tiny_image': product['tiny_image']}
-#     for order in orders_list:
-#         order['image_url'] = product_image_list[int(order['product_no'])]
-#
-#     print(len(orders_list))
-#
-#     return jsonify({'orders': orders_list})
+@api.route('/test/')
+def test():
+    return jsonify({'test': 'hello'})
